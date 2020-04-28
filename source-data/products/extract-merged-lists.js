8f97c5db93c00b9904466922f7030926b5a3d077
esganzerla <esganzerla@gmail.com>
2020-04-28 22:09:12 +0200

    extract products list

---
+++ source-data/products/extract-merged-lists.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const { uniqBy } = require("lodash");
+
+const onlyProducts = require("./products-list.json");
+const consolidatedProducts = require("./products-list-from-consolidated.json");
+
+const mergedList = uniqBy([...onlyProducts, ...consolidatedProducts], "name");
+
+fs.writeFileSync(
+  "./products-lists-merged.json",
+  JSON.stringify(mergedList, null, 2)
+);
