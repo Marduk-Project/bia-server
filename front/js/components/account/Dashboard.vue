781a80edc2b19ac7a58a5e49a7b8e9773f9c6ffd
esganzerla <esganzerla@gmail.com>
2020-05-13 21:03:29 +0200

    Update Account Dashboard

--- front/js/components/account/Dashboard.vue
+++ front/js/components/account/Dashboard.vue
@@ -4,63 +4,34 @@
     <div class="text-center">Seja bem-vindo(a) ao portal de solicitações</div>
     <br />
     <br />
-    <h4>Solicitar demanda ou informar recebimento</h4>
-    <table class="table table-hover table-striped">
-      <thead>
-        <tr>
-          <th>Entidades disponíveis</th>
-          <th>Cidade</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="entity in personContactList" :key="entity.id">
-          <td>{{ entity.person.name }}</td>
-          <td>{{ entity.person.city ? entity.person.city.name : '' }}</td>
-          <td>
-            <button
-              class="btn btn-outline-secondary"
-              type="button"
-              @click="onRequestClick"
-            >
-              <i class="fas fa-clipboard-list"></i> Solicitar
-            </button>
-            <button
-              class="btn btn-outline-secondary ml-1"
-              type="button"
-              @click="onInformReceivedClick"
-            >
-              <i class="fas fa-clipboard-check"></i> Informar recebimento
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <center>
+      <router-link
+        class="btn btn-outline-secondary"
+        tag="a"
+        :to="{ name: 'or_order.create' }"
+      >
+        <i class="fas fa-clipboard-list"></i> Solicitar demanda
+      </router-link>
+      <br />
+      <br />
+      <router-link
+        class="btn btn-outline-secondary ml-1"
+        tag="a"
+        :to="{ name: 'or_order.index' }"
+      >
+        <i class="fas fa-clipboard-check"></i> Visualizar suas demandas
+      </router-link>
+    </center>
   </div>
 </template>
 
 <script>
   import axios from '@mixins/axios-auth';
-  import { apiMixin } from '@mixins/api-mixin';
+  // import { apiMixin } from '@mixins/api-mixin';
   import { mapGetters } from 'vuex';
 
   export default {
-    mixins: [apiMixin],
-    data() {
-      return {
-        account: null,
-        level: 10,
-      };
-    },
-    computed: {
-      ...mapGetters({
-        personContactList: 'getPersonContactList',
-      }),
-    },
-    methods: {
-      onRequestClick() {},
-      onInformReceivedClick() {},
-    },
+    // mixins: [apiMixin],
     mounted() {
       this.$store.dispatch('setTitle', 'Dashboard');
     },
